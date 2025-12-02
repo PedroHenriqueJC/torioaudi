c5faf79fc9d54aa5dee173cade6cb248e73649c5
PedroHenriqueJC <pedrohenrique3317@outlook.com>
2025-12-02 16:42:16 -0300

    :recycle: FIX: equipamento_evento model name

--- src/app/Models/EquipamentoEvento.php
+++ src/app/Models/EquipamentoEvento.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class EquipamentoSala extends Pivot
+class EquipamentoEvento extends Pivot
 {
     protected $table = 'equipamento_evento';
     public $incrementing = false;
